d22b841133eac196bfe21bc58d67a4bbf336a9f9
Yegishetty Vamshi Krishna <yegishettyvamshi@gmail.com>
2022-12-11 17:57:19 +0530

    Assignment 4 First Commit

---
+++ IneuronAssignment4/src/ineuron/VowelCounter.java
@@ -0,0 +1,45 @@
+package ineuron;
+
+
+	import java.util.*;
+	public class VowelCounter {
+
+	public static void main(String[] args) {
+	String line;
+	Scanner sc = new Scanner(System.in);
+	System.out.print("\nEnter the string : ");
+	line = sc.nextLine();
+	int vowels = 0, consonants = 0, digits = 0, spaces = 0, symbols = 0;
+
+	line = line.toLowerCase();
+	for(int i = 0; i < line.length(); ++i)
+	{
+	char ch = line.charAt(i);
+	if(ch == 'a' || ch == 'e'|| ch == 'i'
+	|| ch == 'o' || ch == 'u') {
+	++vowels;
+	}
+	else if((ch >= 'a'&& ch <= 'z')) {
+	++consonants;
+	}
+	else if( ch >= '0' && ch <= '9')
+	{
+	++digits;
+	}
+	else if (ch == ' ')
+	{
+	++spaces;
+	}
+	else
+	++symbols;
+	}
+
+	System.out.println( "Vowels :"+ vowels);
+	System.out.println("Constants :" + consonants);
+	System.out.println("Digits :" + digits);
+	System.out.println("White Spaces :" + spaces);
+	System.out.println("Symbols :"+ symbols);
+	}
+	}
+
+
---
+++ IneuronAssignment4/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author vamshikrishna
+ *
+ */
+module IneuronAssignment4 {
+}
